fe63d9bd5f9addac033f134c7da441a4490a1397
Md-Mansor <mansor.me777@ gmail.com>
2023-12-25 19:51:41 +0600

    cors policy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'red-owner.surge.sh',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
